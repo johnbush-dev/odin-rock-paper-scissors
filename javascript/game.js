9a2e90724189d0833f29a4e30205cbc96847616a
johnbush-dev <jm-bush-developer@outlook.com>
2023-08-03 00:58:35 +0100

    Logic errors fixed the code wil now correctly display the results instead of winning

--- javascript/game.js
+++ javascript/game.js
@@ -1,35 +1,61 @@
 function mainMethod() {
-   let userInput = prompt("Please choose either: Rock, Paper or Scissors: ")
-    playRockPaperScissors(userInput, getComputerChoice() ? alert("You win!") : alert("You lose!"));
-    mainMethod();
+    let userInput = prompt("Please choose either: Rock, Paper, or Scissors: ");
+    let userChoice = convertUserChoice(userInput);
+    
+    if (userChoice !== null) {
+        let computerChoice = getComputerChoice();
+        let result = playRockPaperScissors(userChoice, computerChoice);
+
+        if (result === "win") {
+            alert(`You win! You chose ${userInput} and the computer chose ${convertComputerChoice(computerChoice)}.`);
+        } else if (result === "lose") {
+            alert(`You lose! You chose ${userInput} and the computer chose ${convertComputerChoice(computerChoice)}.`);
+        } else {
+            alert(`It's a tie! You chose ${userInput} and the computer chose ${convertComputerChoice(computerChoice)}.`);
+        }
+    } else {
+        alert(`Invalid input! Please choose either: Rock, Paper, or Scissors.`);
+    }
+    
+    mainMethod(); 
 }
+
 function getComputerChoice() {
-    return parseInt(Math.floor(Math.random() * (3 - 1 + 1) + 1))
-    // Rock = 1, Paper = 2, Scissors = 3
+    return Math.floor(Math.random() * 3) + 1; 
 }
-function playRockPaperScissors(userChoice, getComputerChoice){
-    if (userChoice.toUpperCase() === "rock".toUpperCase()) {
-        // Rock
-        userChoice = 1;
-        if (userChoice === getComputerChoice) {
-            return true;
-        }
-        return false;
-    } else if (userChoice.toUpperCase() === "paper".toUpperCase()){
-        // Paper
-        userChoice = 2;
-        if (userChoice === getComputerChoice) {
-            return true;
-        }
-        return false;
-    } else if (userChoice.toUpperCase() === "scissors".toUpperCase()){
-        // Scissors
-        userChoice = 3;
-        if (userChoice === getComputerChoice) {
-            return true;
-        }
-        return false;
-    } else     
-    console.log("Invalid Input!")
-    return false;
+
+function convertUserChoice(userInput) {
+    userInput = userInput.trim().toLowerCase();
+    if (userInput === "rock") {
+        return 1;
+    } else if (userInput === "paper") {
+        return 2;
+    } else if (userInput === "scissors") {
+        return 3;
+    }
+    return null;
+}
+
+function convertComputerChoice(computerChoice) {
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if (computerChoice === 2) {
+        return "Paper";
+    } else if (computerChoice === 3) {
+        return "Scissors";
+    }
+}
+
+function playRockPaperScissors(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "tie";
+    } else if (
+        (userChoice === 1 && computerChoice === 3) ||
+        (userChoice === 2 && computerChoice === 1) ||
+        (userChoice === 3 && computerChoice === 2)
+    ) {
+        return "win";
+    } else {
+        return "lose";
+    }
 }
